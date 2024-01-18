ad0c399f7dd39c3864d384749c41c3166d2b2ff4
Onella Natalie <onellanatalie824@gmail.com>
2024-01-18 09:59:47 +0530

    counter -vue

---
+++ vue-counter/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {ref} from "vue";
+
+const count = ref(0);
+</script>
+<template>
+  <main>
+  <div style = "text-align: center;">
+    <h4>the current count is ...</h4>
+    <h1>{{count}}</h1>
+    <button @click="count--">-</button>
+    <button @click="count++">+</button>
+  </div>
+</main>
+</template>
+<style scoped>
+main{
+  background-color:aliceblue;
+  display: flex;
+  justify-content: center;
+}
+</style>
